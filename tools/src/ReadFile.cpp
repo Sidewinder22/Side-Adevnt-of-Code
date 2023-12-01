8bc7dc4514e279ecfc56aee1ac5372f1087fbd3a
Sidewinder22 <sidewinder22@protonmail.com>
2023-12-01 17:21:48 +0100

    Add tools library

---
+++ tools/src/ReadFile.cpp
@@ -0,0 +1,23 @@
+#include "ReadFile.hpp"
+    
+ReadFile::ReadFile(std::string fileName)
+    : fileName_(fileName)
+{ }
+
+std::vector<std::string> ReadFile::read()
+{
+    std::ifstream file(fileName_);
+
+    if (!file.is_open())
+    {
+        throw std::runtime_error("Can't open file: " + fileName_);
+    }
+
+    std::vector<std::string> lines;
+    for (std::string line; std::getline(file, line);)
+    {
+        lines.push_back(line);
+    }
+
+    return lines;
+}
