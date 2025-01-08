44bd0b4cafbcbb13cbc1f66640f802f56a1275c8
Damian Stępień <dstepien.dev@gmail.com>
2025-01-08 13:22:45 +0100

    2024 3 part2

--- 2024/3/part_1.cpp
+++ 2024/3/part_1.cpp
@@ -3,32 +3,25 @@
 #include <regex>
 
 import filesystem;
+import numbers;
 
 int main()
 {
     auto reader = filesystem::ReadFile("../2024/3/input");
-    // auto reader = filesystem::ReadFile("../2024/3/test_input");
-    auto lines = reader.readLines();
+    auto lines  = reader.readLines();
 
     std::regex mulRegex(R"(mul\(\d{1,3},\d{1,3}\))");
-    std::regex numRegex(R"(\d{1,3})");
 
     int result = 0;
     for (auto&& line : lines)
     {
         for (std::smatch mulSMatch; std::regex_search(line, mulSMatch, mulRegex);)
         {
-            std::string      mulPhrase = mulSMatch.str();
-            std::vector<int> numbers;
+            std::string mulPhrase = mulSMatch.str();
+            auto        numbers   = numbers::extractNumbers(mulPhrase);
 
-            for (std::smatch numSMatch; std::regex_search(mulPhrase, numSMatch, numRegex);)
-            {
-                numbers.push_back(std::stoi(numSMatch.str()));
-                mulPhrase = numSMatch.suffix();
-            }
-
-            line = mulSMatch.suffix();
             result += numbers[0] * numbers[1];
+            line = mulSMatch.suffix();
         }
     }
 
